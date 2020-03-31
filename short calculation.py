3a8d537161d86a61ce23bc6adefb9f9447c7b542
Mursal Furqan <36442744+mursalfk@users.noreply.github.com>
2020-04-01 00:29:49 +0500

    Added New Operators
    Added Modulo and Floor Operator and also added a comment on the exponential operator

--- short calculation.py
+++ short calculation.py
@@ -1,5 +1,7 @@
-print(8/9)#divided
-print(6*4)#multiply
-print(1/2+(4/3)-2)#bodmas
-print(2%2)#remainder
-print( 4**6)
+print(8/9) #Division Operator
+print(6*4) #Multiplication Operator
+print(1/2+(4/3)-2) #BODMAS Operator
+print(2%2) #Remainder Operator
+print(4**6) #Exponential Operator
+print(4 % 2) #Modulo Operator
+print(10 // 3) #Floor Division
